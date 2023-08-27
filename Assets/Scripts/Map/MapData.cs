a706f8544b87e390bdaad97dbdf12ed9da987d7e
Daemon <daerhoy@icloud.com>
2023-08-27 21:17:16 +0800

    update

---
+++ Assets/Scripts/Map/MapData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class UnitData
+{
+
+    string name;    // 名称
+    int hp, mp;     // 血量，行动点
+    string orientation; // 朝向
+}
+
+/// <summary>
+/// 地图信息
+/// </summary>
+[SerializeField]
+public class MapData
+{
+    
+}
