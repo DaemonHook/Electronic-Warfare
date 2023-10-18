831854225e18b3a1a7b3c24771f348d753fc4f1d
hook <116503270+DaemonHook@users.noreply.github.com>
2023-10-18 21:08:28 +0800

    test

--- Assets/Scripts/Title/StartView.cs
+++ Assets/Scripts/Title/StartView.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartView : MonoBehaviour
 {
+
+    public void OnStartButton()
+    {
+        
+    }
     // Start is called before the first frame update
     void Start()
     {
