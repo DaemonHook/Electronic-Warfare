1c392ca5b9313ac34c9eeb53d08edd3b08f8a37e
hook <116503270+DaemonHook@users.noreply.github.com>
2024-02-16 21:47:52 +0800

    update

--- Assets/Scripts/Utils/Singleton.cs
+++ Assets/Scripts/Utils/Singleton.cs
@@ -1,4 +1,23 @@
-public class Singleton<T>
+using System;
+using UnityEngine;
+
+/// <summary>
+/// 继承自 MonoBehaviour 的单例
+/// </summary>
+public class MonoBehaviourSingleton<T> : MonoBehaviour where T : MonoBehaviourSingleton<T> 
+// 保证可以在这里使用MonoBehaviourSingleton的方法
 {
-    
+    /// <summary>
+    /// 访问单例的唯一接口
+    /// </summary>
+    public static T I   // 我嫌 Instance 单词太长了
+    {
+        get;
+        private set;
+    }
+
+    public void Awake()
+    {
+        I = this as T;
+    }
 }
